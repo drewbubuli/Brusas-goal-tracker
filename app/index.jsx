7ad4ee4d497e067a3340235d2df34d371a79adb6
“drewbubuli” <brusasandrew2604.com>
2025-08-22 16:46:21 +0800

    index is changed

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        M Y  G O A L S
+        M Y D R U M A P P
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
